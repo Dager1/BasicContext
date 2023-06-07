7321fa781e65e629ae6ada3ce3dc9e5032e7f9cc
f <franklinrohan2002@gmail.com>
2023-06-07 12:07:08 +0530

    chore:conditional to show profile or login

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { useState } from "react";
 
 import "./App.css";
 import Login from "./componets/Login";
+import Profile from "./componets/Profile";
 
 function App() {
-  return (
-    <div>
-      <Login />
-    </div>
-  );
+  const [showProfile, setShowProfile] = useState(false);
+  return <div>{showProfile ? <Profile /> : <Login />}</div>;
 }
 
 export default App;
