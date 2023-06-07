17733b863d16f6f08add4a1ed67d52b6b6254670
f <franklinrohan2002@gmail.com>
2023-06-07 12:40:36 +0530

    fix:called the context

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import { useState } from "react";
 import "./App.css";
 import Login from "./componets/Login";
 import Profile from "./componets/Profile";
-import LoginContext from "./context/LoginContext";
+import { LoginContext } from "./context/LoginContext";
 
 function App() {
   const [showProfile, setShowProfile] = useState(false);
+  const [name, setName] = useState("");
   return (
     <div>
-      <LoginContext.Provider>
+      <LoginContext.Provider value={{ name, setName, setShowProfile }}>
         {showProfile ? <Profile /> : <Login />}
       </LoginContext.Provider>
     </div>
