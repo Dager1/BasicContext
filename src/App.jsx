225e46d6391f5c2beeb17619d3700e6fb2a1d07d
f <franklinrohan2002@gmail.com>
2023-06-07 12:15:04 +0530

    chore:made called the context to mange the states

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,17 @@ import { useState } from "react";
 import "./App.css";
 import Login from "./componets/Login";
 import Profile from "./componets/Profile";
+import LoginContext from "./context/LoginContext";
 
 function App() {
   const [showProfile, setShowProfile] = useState(false);
-  return <div>{showProfile ? <Profile /> : <Login />}</div>;
+  return (
+    <div>
+      <LoginContext.Provider>
+        {showProfile ? <Profile /> : <Login />}
+      </LoginContext.Provider>
+    </div>
+  );
 }
 
 export default App;
