b63230c5f5bae48324b6738b91fd64b7f46192fb
f <franklinrohan2002@gmail.com>
2023-06-07 12:06:30 +0530

    refactor:not to show username

--- src/componets/Login.jsx
+++ src/componets/Login.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 function Login() {
   const [name, setName] = useState("");
-  const [showProfile, setShowProfile] = useState(false);
 
   return (
     <div>
@@ -19,16 +18,9 @@ function Login() {
           <label>Password:</label>
           <input type="password" />
           <br />
-          <button
-            onClick={() => {
-              setShowProfile(true);
-              e.preventDefault();
-            }}>
-            Click
-          </button>
+          <button>Click</button>
         </div>
       </form>
-      {showProfile && <h1>{name}</h1>}
     </div>
   );
 }
