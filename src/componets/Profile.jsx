11800dbfe1d5fe296e2e0d7ee998d5d016c489bc
f <franklinrohan2002@gmail.com>
2023-06-07 12:39:21 +0530

    chore:call the name state

--- src/componets/Profile.jsx
+++ src/componets/Profile.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LoginContext } from "../context/LoginContext";
 
 function Profile() {
+  const { name } = useContext(LoginContext);
   return (
     <div>
       <h1>Profile</h1>
-      <h2>UserName:</h2>
+      <h2>UserName:{name}</h2>
     </div>
   );
 }
